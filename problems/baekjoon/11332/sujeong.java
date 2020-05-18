5e287add145fa3b89f5bb40e831571db09e2c337
2ssue <e2ssue@gmail.com>
2020-05-18 21:48:40 +0900

    Refactor: 계산 부분 함수화 및 비교 로직 변경
    - String을 switch로 비교할 수 있다는 것을 알게 되어 방식 변경
    - continue 보다는 early return 문을 사용할 수 있도록 함수화

--- problems/baekjoon/11332/sujeong.java
+++ problems/baekjoon/11332/sujeong.java
@@ -6,8 +6,6 @@ import java.io.OutputStreamWriter;
 import java.math.BigInteger;
 
 public class Main {
-    private static final String FAIL = "TLE!\n";
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -16,52 +14,65 @@ public class Main {
 
         for (int i = 0; i < testCase; i++) {
             String[] situation = br.readLine().split(" ");
-            String bigO = situation[0];
-            BigInteger inputLength = new BigInteger(situation[1]);
-            BigInteger executeCount = new BigInteger(situation[2]);
-            BigInteger timeLimit = BigInteger.valueOf(Integer.parseInt(situation[3]) * 100000000);
 
-            if (bigO.equals("O(N)")) {
-                if (inputLength.multiply(executeCount).compareTo(timeLimit) == 1) {
-                    bw.write(FAIL);
-                    continue;
-                }
-            } else if (bigO.equals("O(N^2)")) {
-                if (inputLength.pow(2).multiply(executeCount).compareTo(timeLimit) == 1) {
-                    bw.write(FAIL);
-                    continue;
-                }
-            } else if (bigO.equals("O(N^3)")) {
-                if (inputLength.pow(3).multiply(executeCount).compareTo(timeLimit) == 1) {
-                    bw.write(FAIL);
-                    continue;
-                }
-            } else if (bigO.equals("O(2^N)")) {
-                BigInteger base = new BigInteger("2");
-                if (base.pow(inputLength.intValue()).multiply(executeCount).compareTo(timeLimit) == 1) {
-                    bw.write(FAIL);
-                    continue;
-                }
+            if (isTimeout(situation)) {
+                bw.write("TLE!\n");
             } else {
-                int n = inputLength.intValue();
-
-                while (n-- > 1) {
-                    inputLength = inputLength.multiply(new BigInteger(Integer.toString(n)));
-                    if (inputLength.compareTo(timeLimit) == 1) {
-                        break;
-                    }
-                }
-
-                if (inputLength.multiply(executeCount).compareTo(timeLimit) == 1) {
-                    bw.write(FAIL);
-                    continue;
-                }
+                bw.write("May Pass.\n");
             }
 
-            bw.write("May Pass.\n");
         }
 
         bw.flush();
         bw.close();
+    }
+
+    private static boolean isTimeout(String[] inputs) {
+        String bigO = inputs[0];
+        BigInteger inputLength = new BigInteger(inputs[1]);
+        BigInteger executeCount = new BigInteger(inputs[2]);
+        BigInteger timeLimit = BigInteger.valueOf(Integer.parseInt(inputs[3]) * 100000000);
+
+        switch (bigO) {
+            case "O(N)":
+                if (inputLength.multiply(executeCount).compareTo(timeLimit) == 1) {
+                    return true;
+                }
+                break;
+            case "O(N^2)":
+                if (inputLength.pow(2).multiply(executeCount).compareTo(timeLimit) == 1) {
+                    return true;
+                }
+                break;
+            case "O(N^3)":
+                if (inputLength.pow(3).multiply(executeCount).compareTo(timeLimit) == 1) {
+                    return true;
+                }
+                break;
+            case "O(2^N)":
+                if (BigInteger.valueOf(2).pow(inputLength.intValue()).multiply(executeCount)
+                        .compareTo(timeLimit) == 1) {
+                    return true;
+                }
+                break;
+            case "O(N!)":
+                int n = inputLength.intValue();
+
+                while (n-- > 1) {
+                    inputLength = inputLength.multiply(BigInteger.valueOf(n));
+
+                    if (inputLength.compareTo(timeLimit) == 1) {
+                        return true;
+                    }
+                }
+
+                if (inputLength.multiply(executeCount).compareTo(timeLimit) == 1) {
+                    return true;
+                }
+
+                break;
+        }
+
+        return false;
     }
 }
