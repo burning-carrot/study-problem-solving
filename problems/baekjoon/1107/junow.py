ffb91c13a6c6a507b5a01c0ae67bbf3836c92bd2
junow <taristmas@gmail.com>
2020-05-22 19:08:24 +0900

    Add junow-boj-1107

--- problems/baekjoon/1107/junow.py
+++ problems/baekjoon/1107/junow.py
@@ -7,22 +7,17 @@ def check(num, broken):
       return False
   return True
 
-def bf(N, broken):
-  minClick=1000001
-  for num in range(1000001):
-    if check(num, broken):
-      minClick = min(minClick, abs(N-num) + len(str(N)))
-
-  return minClick
-
 N = int(stdin.readline())
 M = int(stdin.readline())
 broken = set()
 answer = abs(N-100)
-if M>0:
-  broken = set(map(int, stdin.readline().split()))
-  answer = min(answer, bf(N, broken))
-  print(answer)
-else:
-  answer = min(abs(len(str(N))), abs(100-N))
-  print(answer)
+
+broken = set(map(int, stdin.readline().split()))
+
+for num in range(1000001):
+  cur = str(num)
+  if check(cur, broken):
+    answer = min(answer, abs(num-N) + len(cur))
+
+
+print(answer)
